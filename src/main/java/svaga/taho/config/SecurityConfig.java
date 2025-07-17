004abf1bd793bf600b2bb9d1af2a3652fcd5024d
Mihail-Tsaletov <165092350+Mihail-Tsaletov@users.noreply.github.com>
2025-07-17 19:05:13 +0300

    firebase доступ к запросам с выкл защитой

--- src/main/java/svaga/taho/config/SecurityConfig.java
+++ src/main/java/svaga/taho/config/SecurityConfig.java
@@ -5,12 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import svaga.taho.firebase.FirebaseAuthFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/firebase/test").permitAll()
+                        .requestMatchers("api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new FirebaseAuthFilter(), UsernamePasswordAuthenticationFilter.class);
 
-
+        return http.build();
+    }
 
 
 }
