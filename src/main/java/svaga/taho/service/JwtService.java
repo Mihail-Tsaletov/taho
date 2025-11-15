5b780eaa5981b76253121dfa11d39ce86f1facf7
Mihail-Tsaletov <165092350+Mihail-Tsaletov@users.noreply.github.com>
2025-11-15 21:51:49 +0300

    обновил секрет

--- src/main/java/svaga/taho/service/JwtService.java
+++ src/main/java/svaga/taho/service/JwtService.java
@@ -3,7 +3,9 @@ package svaga.taho.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -13,27 +15,32 @@ import java.util.Map;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    @Value("${jwt.expiration}")
+    private long expiration;
 
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
     public String generateToken(String phone, String role) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("role", role);
 
-        // месяц жизни токена
-        long expirationMs = 30L * 24 * 60 * 60 * 1000;
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(phone)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
-                .signWith(key)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
                 .compact();
     }
 
     public Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
